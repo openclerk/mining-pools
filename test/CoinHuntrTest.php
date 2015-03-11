d73770897c9fc9986c89f6efae547165a3e90b2f
Jevon Wright <jevon@jevon.org>
2015-03-11 14:53:05 +1300

    Add CoinHuntr disabled pool
    Issue #401, issue #435

---
+++ test/CoinHuntrTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Account\MiningPool\Tests;
+
+use Monolog\Logger;
+use Account\AccountType;
+use Account\AccountFetchException;
+use Account\Tests\AbstractAccountTest;
+use Account\Tests\AbstractActiveAccountTest;
+use Openclerk\Config;
+use Openclerk\Currencies\Currency;
+
+/**
+ * Tests the {@link CoinHuntr} account type.
+ */
+class CoinHuntrTest extends AbstractAccountTest {
+
+  function __construct() {
+    parent::__construct(new \Account\MiningPool\CoinHuntr());
+  }
+
+}
