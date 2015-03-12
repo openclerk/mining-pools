5c2b39eef65c76d6df0019067c1b3c6f7c92a765
Jevon Wright <jevon@jevon.org>
2015-03-12 16:24:02 +1300

    Add disabled MiningPool.co mining pool
    Issue #401, issue #442

---
+++ src/MiningPoolCo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Account\MiningPool;
+
+use \Monolog\Logger;
+use \Account\Account;
+use \Account\Miner;
+use \Account\DisabledAccount;
+use \Account\SimpleAccountType;
+use \Account\AccountFetchException;
+use \Apis\FetchHttpException;
+use \Apis\Fetch;
+use \Openclerk\Currencies\CurrencyFactory;
+
+/**
+ * Represents the MiningPool.co mining pool.
+ */
+class MiningPoolCo extends SimpleAccountType implements Miner, DisabledAccount {
+
+  public function getName() {
+    return "MiningPool.co";
+  }
+
+  public function getCode() {
+    return "miningpoolco";
+  }
+
+  public function getURL() {
+    return "https://www.miningpool.co/";
+  }
+
+  public function getFields() {
+    return array(
+      // looks like a 40 character hex string
+      'api_token' => array(
+        'title' => "API key",
+        'regexp' => "#^[a-f0-9]{40}$#",
+      ),
+    );
+  }
+
+  public function fetchSupportedCurrencies(CurrencyFactory $factory, Logger $logger) {
+    return array('dog', 'ltc', 'mec');
+  }
+
+  public function fetchSupportedHashrateCurrencies(CurrencyFactory $factory, Logger $logger) {
+    return array('dog', 'ltc', 'mec');
+  }
+
+  public function fetchBalances($account, CurrencyFactory $factory, Logger $logger) {
+    throw new AccountFetchException("Cannot fetch balances of disabled account");
+  }
+
+  public function disabledAt() {
+    return "2015-03-12";
+  }
+
+}
